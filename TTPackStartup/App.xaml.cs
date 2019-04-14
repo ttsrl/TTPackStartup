94f097699b2f84e2795060a4000ae202ec3c2657
Francesco Fioravanti <francesco.cantante@hotmail.it>
2019-04-15 00:23:53 +0200

    dsa

--- TTPackStartup/App.xaml.cs
+++ TTPackStartup/App.xaml.cs
@@ -3,25 +3,33 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
+using TouchPanels.Devices;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Automation.Peers;
+using Windows.UI.Xaml.Automation.Provider;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI;
+using System.Reflection;
 
 namespace TTPackStartup
 {
-    /// <summary>
-    /// Fornisci un comportamento specifico dell'applicazione in supplemento alla classe Application predefinita.
-    /// </summary>
     sealed partial class App : Application
     {
+        const string CalibrationFilename = "TSC2046";
+        private Tsc2046 tsc2046;
+        private TouchPanels.TouchProcessor processor;
+        private Point lastPosition = new Point(double.NaN, double.NaN);
+
         /// <summary>
         /// Inizializza l'oggetto Application singleton. Si tratta della prima riga del codice creato
         /// creato e, come tale, corrisponde all'equivalente logico di main() o WinMain().
@@ -70,6 +78,7 @@ namespace TTPackStartup
                 }
                 // Assicurarsi che la finestra corrente sia attiva
                 Window.Current.Activate();
+                InitTouch();
             }
         }
 
@@ -96,5 +105,122 @@ namespace TTPackStartup
             //TODO: salvare lo stato dell'applicazione e arrestare eventuali attività eseguite in background
             deferral.Complete();
         }
+
+        private async void InitTouch()
+        {
+            tsc2046 = await TouchPanels.Devices.Tsc2046.GetDefaultAsync();
+            try
+            {
+                await tsc2046.LoadCalibrationAsync(CalibrationFilename);
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                await CalibrateTouch(); //Initiate calibration if we don't have a calibration on file
+            }
+            catch (System.UnauthorizedAccessException)
+            {
+                //No access to documents folder
+                await new Windows.UI.Popups.MessageDialog("Non è stato possibile avviare il file di configurazione del touchscreen", "Errore").ShowAsync();
+                throw;
+            }
+            //Load up the touch processor and listen for touch events
+            processor = new TouchPanels.TouchProcessor(tsc2046);
+            processor.PointerDown += Processor_PointerDown;
+            processor.PointerMoved += Processor_PointerMoved;
+            processor.PointerUp += Processor_PointerUp;
+        }
+
+        IScrollProvider currentScrollItem;
+
+        private void Processor_PointerDown(object sender, TouchPanels.PointerEventArgs e)
+        {
+            currentScrollItem = FindElementsToInvoke(e.Position);
+            lastPosition = e.Position;
+        }
+        private void Processor_PointerMoved(object sender, TouchPanels.PointerEventArgs e)
+        {
+            if (currentScrollItem != null)
+            {
+                double dx = e.Position.X - lastPosition.X;
+                double dy = e.Position.Y - lastPosition.Y;
+                if (!currentScrollItem.HorizontallyScrollable) dx = 0;
+                if (!currentScrollItem.VerticallyScrollable) dy = 0;
+
+                Windows.UI.Xaml.Automation.ScrollAmount h = Windows.UI.Xaml.Automation.ScrollAmount.NoAmount;
+                Windows.UI.Xaml.Automation.ScrollAmount v = Windows.UI.Xaml.Automation.ScrollAmount.NoAmount;
+                if (dx < 0) h = Windows.UI.Xaml.Automation.ScrollAmount.SmallIncrement;
+                else if (dx > 0) h = Windows.UI.Xaml.Automation.ScrollAmount.SmallDecrement;
+                if (dy < 0) v = Windows.UI.Xaml.Automation.ScrollAmount.SmallIncrement;
+                else if (dy > 0) v = Windows.UI.Xaml.Automation.ScrollAmount.SmallDecrement;
+                currentScrollItem.Scroll(h, v);
+            }
+            lastPosition = e.Position;
+        }
+        private void Processor_PointerUp(object sender, TouchPanels.PointerEventArgs e)
+        {
+            currentScrollItem = null;
+        }
+
+        private bool _isCalibrating = false; //flag used to ignore the touch processor while calibrating
+        private async Task CalibrateTouch()
+        {
+            _isCalibrating = true;
+            var calibration = await TouchPanels.UI.LcdCalibrationView.CalibrateScreenAsync(tsc2046);
+            _isCalibrating = false;
+            tsc2046.SetCalibration(calibration.A, calibration.B, calibration.C, calibration.D, calibration.E, calibration.F);
+            try
+            {
+                await tsc2046.SaveCalibrationAsync(CalibrationFilename);
+            }
+            catch (Exception ex)
+            {
+                await new Windows.UI.Popups.MessageDialog("Non è stato possibile avviare la calibrazione del touchscreen", "Errore").ShowAsync();
+            }
+        }
+
+        private IScrollProvider FindElementsToInvoke(Point screenPosition)
+        {
+            if (_isCalibrating) return null;
+
+            var elements = VisualTreeHelper.FindElementsInHostCoordinates(new Windows.Foundation.Point(screenPosition.X, screenPosition.Y), Window.Current.Content, false);
+            //Search for buttons in the visual tree that we can invoke
+            //If we can find an element button that implements the 'Invoke' automation pattern (usually buttons), we'll invoke it
+            foreach (var e in elements.OfType<FrameworkElement>())
+            {
+                var element = e;
+                AutomationPeer peer = null;
+                object pattern = null;
+                while (true)
+                {
+                    peer = FrameworkElementAutomationPeer.FromElement(element);
+                    if (peer != null)
+                    {
+                        pattern = peer.GetPattern(PatternInterface.Invoke);
+                        if (pattern != null)
+                        {
+                            break;
+                        }
+                        pattern = peer.GetPattern(PatternInterface.Scroll);
+                        if (pattern != null)
+                        {
+                            break;
+                        }
+                    }
+                    var parent = VisualTreeHelper.GetParent(element);
+                    if (parent is FrameworkElement)
+                        element = parent as FrameworkElement;
+                    else
+                        break;
+                }
+                if (pattern != null)
+                {
+                    var p = pattern as Windows.UI.Xaml.Automation.Provider.IInvokeProvider;
+                    p?.Invoke();
+                    return pattern as IScrollProvider;
+                }
+            }
+            return null;
+        }
+
     }
 }
