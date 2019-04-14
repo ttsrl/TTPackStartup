c549f5d430d12e905a4910ede4eda7ae46da6818
Francesco Fioravanti <francesco.cantante@hotmail.it>
2019-04-14 15:47:54 +0200

    first commit

---
+++ TTPackStartup/MainPage.xaml.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using TouchPanels.Devices;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Gpio;
+using Windows.UI.Xaml.Automation.Provider;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Automation.Peers;
+using Windows.UI;
+
+// Il modello di elemento Pagina vuota è documentato all'indirizzo https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x410
+
+namespace TTPackStartup
+{
+    /// <summary>
+    /// Pagina vuota che può essere usata autonomamente oppure per l'esplorazione all'interno di un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private const int LED_PIN = 6;
+        private GpioPin Led;
+
+        const string CalibrationFilename = "TSC2046";
+        private Tsc2046 tsc2046;
+        private TouchPanels.TouchProcessor processor;
+        private Point lastPosition = new Point(double.NaN, double.NaN);
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            InitGPIO();
+            Init();
+        }
+
+        private void InitGPIO()
+        {
+            var gpio = GpioController.GetDefault();
+
+            if (gpio == null)
+            {
+                return;
+            }
+
+            Led = gpio.OpenPin(LED_PIN);
+
+            Led.Write(GpioPinValue.High);
+            Led.SetDriveMode(GpioPinDriveMode.Output);
+
+        }
+
+        private async void Init()
+        {
+            tsc2046 = await TouchPanels.Devices.Tsc2046.GetDefaultAsync();
+            try
+            {
+                await tsc2046.LoadCalibrationAsync(CalibrationFilename);
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                await CalibrateTouch(); //Initiate calibration if we don't have a calibration on file
+            }
+            catch (System.UnauthorizedAccessException)
+            {
+                //No access to documents folder
+                await new Windows.UI.Popups.MessageDialog("Make sure the application manifest specifies access to the documents folder and declares the file type association for the calibration file.", "Configuration Error").ShowAsync();
+                throw;
+            }
+            //Load up the touch processor and listen for touch events
+            processor = new TouchPanels.TouchProcessor(tsc2046);
+            processor.PointerDown += Processor_PointerDown;
+            processor.PointerMoved += Processor_PointerMoved;
+            processor.PointerUp += Processor_PointerUp;
+        }
+
+        IScrollProvider currentScrollItem;
+
+        private void Processor_PointerDown(object sender, TouchPanels.PointerEventArgs e)
+        {
+            currentScrollItem = FindElementsToInvoke(e.Position);
+            lastPosition = e.Position;
+        }
+        private void Processor_PointerMoved(object sender, TouchPanels.PointerEventArgs e)
+        {
+            if (currentScrollItem != null)
+            {
+                double dx = e.Position.X - lastPosition.X;
+                double dy = e.Position.Y - lastPosition.Y;
+                if (!currentScrollItem.HorizontallyScrollable) dx = 0;
+                if (!currentScrollItem.VerticallyScrollable) dy = 0;
+
+                Windows.UI.Xaml.Automation.ScrollAmount h = Windows.UI.Xaml.Automation.ScrollAmount.NoAmount;
+                Windows.UI.Xaml.Automation.ScrollAmount v = Windows.UI.Xaml.Automation.ScrollAmount.NoAmount;
+                if (dx < 0) h = Windows.UI.Xaml.Automation.ScrollAmount.SmallIncrement;
+                else if (dx > 0) h = Windows.UI.Xaml.Automation.ScrollAmount.SmallDecrement;
+                if (dy < 0) v = Windows.UI.Xaml.Automation.ScrollAmount.SmallIncrement;
+                else if (dy > 0) v = Windows.UI.Xaml.Automation.ScrollAmount.SmallDecrement;
+                currentScrollItem.Scroll(h, v);
+            }
+            lastPosition = e.Position;
+        }
+        private void Processor_PointerUp(object sender, TouchPanels.PointerEventArgs e)
+        {
+            currentScrollItem = null;
+        }
+
+        private bool _isCalibrating = false; //flag used to ignore the touch processor while calibrating
+        private async Task CalibrateTouch()
+        {
+            _isCalibrating = true;
+            var calibration = await TouchPanels.UI.LcdCalibrationView.CalibrateScreenAsync(tsc2046);
+            _isCalibrating = false;
+            tsc2046.SetCalibration(calibration.A, calibration.B, calibration.C, calibration.D, calibration.E, calibration.F);
+            try
+            {
+                await tsc2046.SaveCalibrationAsync(CalibrationFilename);
+            }
+            catch (Exception ex)
+            {
+                //Status.Text = ex.Message;
+            }
+        }
+
+        private IScrollProvider FindElementsToInvoke(Point screenPosition)
+        {
+            if (_isCalibrating) return null;
+
+            var elements = VisualTreeHelper.FindElementsInHostCoordinates(new Windows.Foundation.Point(screenPosition.X, screenPosition.Y), this, false);
+            //Search for buttons in the visual tree that we can invoke
+            //If we can find an element button that implements the 'Invoke' automation pattern (usually buttons), we'll invoke it
+            foreach (var e in elements.OfType<FrameworkElement>())
+            {
+                var element = e;
+                AutomationPeer peer = null;
+                object pattern = null;
+                while (true)
+                {
+                    peer = FrameworkElementAutomationPeer.FromElement(element);
+                    if (peer != null)
+                    {
+                        pattern = peer.GetPattern(PatternInterface.Invoke);
+                        if (pattern != null)
+                        {
+                            break;
+                        }
+                        pattern = peer.GetPattern(PatternInterface.Scroll);
+                        if (pattern != null)
+                        {
+                            break;
+                        }
+                    }
+                    var parent = VisualTreeHelper.GetParent(element);
+                    if (parent is FrameworkElement)
+                        element = parent as FrameworkElement;
+                    else
+                        break;
+                }
+                if (pattern != null)
+                {
+                    var p = pattern as Windows.UI.Xaml.Automation.Provider.IInvokeProvider;
+                    p?.Invoke();
+                    return pattern as IScrollProvider;
+                }
+            }
+            return null;
+        }
+
+        private void LED_Click(object sender, RoutedEventArgs e)
+        {
+            if (Led.Read() == GpioPinValue.High)
+            {
+                Button1.Background = new SolidColorBrush(Color.FromArgb(255, 49, 115, 255));
+                Led.Write(GpioPinValue.Low);
+            }
+            else
+            {
+                Led.Write(GpioPinValue.High);
+                Button1.Background = new SolidColorBrush(Color.FromArgb(255, 160, 160, 160));
+            }
+        }
+    }
+}
