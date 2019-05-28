504ebca44e0774fe59de726c79cda45bdd340c74
Francesco Fioravanti <francesco.cantante@hotmail.it>
2019-05-28 17:41:03 +0200

    keyboard

--- TTPackStartup/NumericKeyboard.xaml.cs
+++ TTPackStartup/NumericKeyboard.xaml.cs
@@ -26,5 +26,74 @@ namespace TTPackStartup
         {
             this.InitializeComponent();
         }
+
+        private void n9_Click(object sender, RoutedEventArgs e)
+        {
+            textbox.Text += "9";
+        }
+
+        private void n8_Click(object sender, RoutedEventArgs e)
+        {
+            textbox.Text += "8";
+        }
+
+        private void n7_Click(object sender, RoutedEventArgs e)
+        {
+            textbox.Text += "7";
+        }
+
+        private void n6_Click(object sender, RoutedEventArgs e)
+        {
+            textbox.Text += "6";
+        }
+
+        private void n5_Click(object sender, RoutedEventArgs e)
+        {
+            textbox.Text += "5";
+        }
+
+        private void n4_Click(object sender, RoutedEventArgs e)
+        {
+            textbox.Text += "4";
+        }
+
+        private void n3_Click(object sender, RoutedEventArgs e)
+        {
+            textbox.Text += "3";
+        }
+
+        private void n2_Click(object sender, RoutedEventArgs e)
+        {
+            textbox.Text += "2";
+        }
+
+        private void n1_Click(object sender, RoutedEventArgs e)
+        {
+            textbox.Text += "1";
+        }
+
+        private void n0_Click(object sender, RoutedEventArgs e)
+        {
+            textbox.Text += "0";
+        }
+
+        private void point_Click(object sender, RoutedEventArgs e)
+        {
+            if(!textbox.Text.Contains("."))
+                textbox.Text += ".";
+        }
+
+        private void sign_Click(object sender, RoutedEventArgs e)
+        {
+            if (textbox.Text.Contains("-"))
+                textbox.Text = textbox.Text.Substring(1);
+            else
+                textbox.Text = "-" + textbox.Text;
+        }
+
+        private void cancel_Click(object sender, RoutedEventArgs e)
+        {
+            textbox.Text = "";
+        }
     }
 }
